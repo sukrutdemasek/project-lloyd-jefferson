b9cbc35d5719c3e5e4faa6ac6dc28c67e7cfa1c0
sukrutdemasek <dima25252525252525@gmail.com>
2024-08-17 21:50:45 +0300

    fixed padding-top and margin

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -9,7 +9,7 @@ import {
   Controller,
 } from 'swiper/modules';
 
-// отримання з API відгуків // 
+// отримання з API відгуків //
 const fetchReviews = async () => {
   const res = await fetch('https://portfolio-js.b.goit.study/api/reviews');
   if (!res.ok) {
@@ -20,7 +20,7 @@ const fetchReviews = async () => {
 
 // створюємо один елемент відгуку //
 function createLiItem({ author, avatar_url, review }) {
-    return `<li class="swiper-slide">
+  return `<li class="swiper-slide">
     <div class="reviews-item">
     <div class="element">
         <img class="review-section-img" src="${avatar_url}" alt="${author}"  />
@@ -33,77 +33,75 @@ function createLiItem({ author, avatar_url, review }) {
 
 // створення списку відгуків //
 function createUlList(arrReviews) {
-    return arrReviews.map(createLiItem).join('');
+  return arrReviews.map(createLiItem).join('');
 }
 
 // основний скрипт по відгукам //
-export const reviewsSwiper = Swiper => { 
-    const section = document.querySelector('.reviews-section');
-    const reviewsBox = section.querySelector('.swiper-wrapper');
-    const reviewsSlider = section.querySelector('.swiper-reviews'); 
-    
-        fetchReviews().then(data => { 
-            const ulList = createUlList(data);
-            reviewsBox.insertAdjacentHTML('beforeend', ulList);              
+export const reviewsSwiper = Swiper => {
+  const section = document.querySelector('.reviews-section');
+  const reviewsBox = section.querySelector('.swiper-wrapper');
+  const reviewsSlider = section.querySelector('.swiper-reviews');
 
-            const swiper = new Swiper(reviewsSlider, {
-                slidesPerView: 1,
-                autoHeight: true,
-                speed: 0,
-                keyboard: {
-                    enabled: true,
-                    onlyInViewport: false,
-                },                
-                breakpoints: {
-                    768: {
-                        speed: 600,
-                        autoHeight: false,
-                        slidesPerView: 2,
-                        spaceBetween: 16,
-                    },
-                    1440: {
-                        speed: 600,
-                        autoHeight: false,
-                        slidesPerView: 4,
-                        spaceBetween: 16,
-                    },
-                },
-                navigation: {
-                    nextEl: section.querySelector('.swiper-button-next'),
-                    prevEl: section.querySelector('.swiper-button-prev'),
-                },
-    
-            });
-            
-        }).catch(err => {
+  fetchReviews()
+    .then(data => {
+      const ulList = createUlList(data);
+      reviewsBox.insertAdjacentHTML('beforeend', ulList);
 
-            iziToast.error({
-                    title: 'Error!',
-                    titleSize: '16',
-                    titleColor: '#fafafa',
-                    message: `Reviews not found`,
-                    messageSize: '16',
-                    messageColor: '#fafafa',
-                    backgroundColor: '#1C1D20',
-                    theme: 'dark',
-                    position: 'bottomCenter',
-                    closeOnEscape: true,
-                    closeOnClick: true,
-                    });
-
-            reviewsBox.innerHTML = '<p class="reviews-notfound">Not found</p>';
+      const swiper = new Swiper(reviewsSlider, {
+        slidesPerView: 1,
+        autoHeight: true,
+        speed: 0,
+        keyboard: {
+          enabled: true,
+          onlyInViewport: false,
+        },
+        breakpoints: {
+          768: {
+            speed: 600,
+            autoHeight: false,
+            slidesPerView: 2,
+            spaceBetween: 16,
+          },
+          1440: {
+            speed: 600,
+            autoHeight: false,
+            slidesPerView: 4,
+            spaceBetween: 16,
+          },
+        },
+        navigation: {
+          nextEl: section.querySelector('.swiper-button-next'),
+          prevEl: section.querySelector('.swiper-button-prev'),
+        },
       });
-}
+    })
+    .catch(err => {
+      iziToast.error({
+        title: 'Error!',
+        titleSize: '16',
+        titleColor: '#fafafa',
+        message: `Reviews not found`,
+        messageSize: '16',
+        messageColor: '#fafafa',
+        backgroundColor: '#1C1D20',
+        theme: 'dark',
+        position: 'bottomCenter',
+        closeOnEscape: true,
+        closeOnClick: true,
+      });
 
+      reviewsBox.innerHTML = '<p class="reviews-notfound">Not found</p>';
+    });
+};
 
 document.addEventListener('DOMContentLoaded', event => {
-    Swiper.use([
-        Autoplay,
-        Navigation,
-        Pagination,
-        Keyboard,
-        EffectFade,
-        Controller,
-    ]);
-    reviewsSwiper(Swiper);
+  Swiper.use([
+    Autoplay,
+    Navigation,
+    Pagination,
+    Keyboard,
+    EffectFade,
+    Controller,
+  ]);
+  reviewsSwiper(Swiper);
 });
