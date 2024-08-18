f27299d0c4e6fa710a3ad495238265eda432aa96
AntonAfanasiev04071990 <aau04071990@gmail.com>
2024-08-18 22:38:58 +0300

    finished

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,14 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const coversSection = document.querySelector('.marquee-inner');
-  const observer = new IntersectionObserver((entries) => {
+  const coversSection = document.querySelector('.covers-section');
+  const imageLines = document.querySelectorAll('.marquee-line');
+  if (!coversSection) {
+    return;
+  }
+  const handleIntersection = entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        coversSection.classList.remove('scrolling');
+        for (let line of imageLines) {
+          line.classList.add('animate');
+        }
       } else {
-        coversSection.classList.add('scrolling');
+        for (let line of imageLines) {
+          line.classList.remove('animate');
+        }
       }
     });
-  }, { threshold: 0.1 });
-
+  };
+  const observer = new IntersectionObserver(handleIntersection, {
+    threshold: 0.2,
+  });
   observer.observe(coversSection);
 });
