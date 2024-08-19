362884fd5bad7a63d41ba8d694ee2bc5ac9b0e81
sukrutdemasek <dima25252525252525@gmail.com>
2024-08-20 01:49:37 +0300

    removed JS animations

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,19 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    function handleScroll() {
-      const elements = document.querySelectorAll('.main-headline, .hero-contact-info');
-      
-      elements.forEach((element) => {
-        const rect = element.getBoundingClientRect();
-        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-  
-        if (rect.top <= windowHeight - 100 && rect.bottom >= 0) {
-          element.classList.add('in-view');
-        } else {
-          element.classList.remove('in-view'); 
-        }
-      });
-    }
+document.addEventListener('DOMContentLoaded', function () {
+  function handleScroll() {
+    const elements = document.querySelectorAll(
+      '.main-headline, .hero-contact-info'
+    );
 
-    window.addEventListener('load', handleScroll);
-    window.addEventListener('scroll', handleScroll);
-  });
+    elements.forEach(element => {
+      const rect = element.getBoundingClientRect();
+      const windowHeight =
+        window.innerHeight || document.documentElement.clientHeight;
+
+      if (rect.top <= windowHeight - 100 && rect.bottom >= 0) {
+        element.classList.add('in-view');
+      } else {
+        element.classList.remove('in-view');
+      }
+    });
+  }
+
+  window.addEventListener('load', handleScroll);
+  window.addEventListener('scroll', handleScroll);
+});
